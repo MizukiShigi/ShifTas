22366b21a5290c365d45c962d58d8119bf3a2e6b
ShigiMizuki <shifi1204@icloud.com>
2021-05-20 23:00:13 +0900

    model追加 createshifts

--- createshifts/models.py
+++ createshifts/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from submitshifts.models import SubmitShift
 
 # Create your models here.
+class ModifyShift(models.Model):
+    submit_id = models.ForeignKey(SubmitShift, on_delete=models.CASCADE)
+    position = models.CharField(max_length=10)
+    modify_fromtime = models.IntegerField()
+    modify_totime = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CounterShift(models.Model):
+    am_1 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_am1')
+    am_2 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_am2')
+    am_3 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_am3')
+    am_4 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_am4')
+    am_5 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_am5')
+    am_6 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_am6')
+    pm_1 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_pm1')
+    pm_2 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_pm2')
+    pm_3 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_pm3')
+    pm_4 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='counter_pm4')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FlyerShift(models.Model):
+    am_1 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='flyer_am1')
+    am_2 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='flyer_am2')
+    pm_1 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='flyer_pm1')
+    pm_2 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='flyer_pm2')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class KitchenShift(models.Model):
+    am_1 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='kitchen_am1')
+    am_2 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='kitchen_am2')
+    am_3 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='kitchen_am3')
+    pm_1 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='kitchen_pm1')
+    pm_2 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='kitchen_pm2')
+    pm_3 = models.ForeignKey(ModifyShift, null=True, on_delete=models.CASCADE, related_name='kitchen_pm3')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CompleteShift(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    counter_shift = models.ForeignKey(CounterShift, on_delete=models.CASCADE)
+    flyer_shift = models.ForeignKey(FlyerShift, on_delete=models.CASCADE)
+    kitchen_shift = models.ForeignKey(KitchenShift, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
--- staff/admin.py
+++ staff/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from staff.models import Staff
 from submitshifts.models import SubmitShift
+from createshifts.models import ModifyShift, CounterShift, FlyerShift, KitchenShift, CompleteShift
 
 # Register your models here.
 admin.site.register(Staff)
 admin.site.register(SubmitShift)
+admin.site.register(ModifyShift)
+admin.site.register(CounterShift)
+admin.site.register(FlyerShift)
+admin.site.register(KitchenShift)
+admin.site.register(CompleteShift)
