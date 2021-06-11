9f456f8bd99e23f1cd208315125e69995085652e
ShigiMizuki <shifi1204@icloud.com>
2021-06-11 21:11:05 +0900

    settings 修正

--- automaticshift/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-# settings/__init__.py
-
-from .production import *           # production.py(本番環境用)をimport
-
-try:
-    from .development import *      # development.py(開発環境用)をimport
-except:
-    pass
--- automaticshift/settings/base.py
+++ automaticshift/settings/base.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'collect_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
