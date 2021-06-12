3913ac295feb91786a5381fe7adf076a6fe3e5d9
ShigiMizuki <shifi1204@icloud.com>
2021-06-12 16:44:12 +0900

    RC版リリース

--- automaticshift/settings.py
+++ automaticshift/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = '(g%ts(jf=hdiw+7-nh)hn33lrllox@5)&f^tqdy$vyw0bk1qcn'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '(g%ts(jf=hdiw+7-nh)hn33lrllox@5)&f^tqdy$vyw0bk1qcn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 # DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
